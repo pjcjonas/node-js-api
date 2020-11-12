9ed0e5313a96da145aa71d5127f4d9342e8c69c6
pjcjonas <pjcjonas@gmail.com>
2020-11-12 07:57:00 +0200

    Added get path, method, querystring and headers

--- index.js
+++ index.js
@@ -11,11 +11,27 @@ const server = http.createServer((req, res) => {
     
     // Get the url and parse it
     const parsedUrl = url.parse(req.url, true);
-    // Get path from url
-    const urlPath = parsedUrl.pathname;
-    // Send the response
     
+    // Get path from url
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, ''); // This removes all trailing slashes
+
+    // Send the response
+    res.end("hello world \n");
+
+    // Get the request Method
+    const method = req.method.toLowerCase();
+
+    // Get the querystring as a object
+    const queryString = parsedUrl.query;
+
+    // Get the headers as an object
+    const headers = req.headers;
+
     // Log what path was requested
+    console.log("request headers: \n\r", headers);
+
+    
 });
 
 // Start the server and that is should listen on port 3000
